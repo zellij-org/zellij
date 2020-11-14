20c944c2ca652998dfe7b113f2c7438ad447ff5a
Aram Drevekenin <aram@poor.dev>
2020-11-14 13:10:35 +0100

    fix(ux): properly echo characters and do not mess up exit

--- src/main.rs
+++ src/main.rs
@@ -341,6 +341,7 @@ pub fn start(mut os_input: Box<dyn OsApi>, opts: Opt) {
     let goto_start_of_last_line = format!("\u{1b}[{};{}H", full_screen_ws.ws_row, 1);
     let goodbye_message = format!("{}\n{}Bye from Mosaic!", goto_start_of_last_line, reset_style);
 
+    os_input.unset_raw_mode(0);
     os_input.get_stdout_writer().write(goodbye_message.as_bytes()).unwrap();
     os_input.get_stdout_writer().flush().unwrap();
 }
