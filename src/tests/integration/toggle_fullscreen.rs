e63343dab494ca4f51cab6d618f7a3bf03352ef6
denis <denismaximov98@gmail.com>
2020-11-16 20:48:59 +0200

    feat: move focus is blocked when pane is in full screen

--- src/tests/integration/toggle_fullscreen.rs
+++ src/tests/integration/toggle_fullscreen.rs
@@ -7,7 +7,7 @@ use crate::{start, Opt};
 
 use crate::tests::utils::commands::{
     CLOSE_FOCUSED_PANE, QUIT, SPLIT_HORIZONTALLY, SPLIT_VERTICALLY,
-    TOGGLE_ACTIVE_TERMINAL_FULLSCREEN,
+    TOGGLE_ACTIVE_TERMINAL_FULLSCREEN, MOVE_FOCUS
 };
 
 fn get_fake_os_input(fake_win_size: &Winsize) -> FakeInputOutput {
@@ -28,6 +28,35 @@ pub fn adding_new_terminal_in_fullscreen() {
         TOGGLE_ACTIVE_TERMINAL_FULLSCREEN,
         SPLIT_HORIZONTALLY,
         CLOSE_FOCUSED_PANE,
+        QUIT,
+    ]);
+    start(Box::new(fake_input_output.clone()), Opt::default());
+
+    let output_frames = fake_input_output
+        .stdout_writer
+        .output_frames
+        .lock()
+        .unwrap();
+    let snapshots = get_output_frame_snapshots(&output_frames, &fake_win_size);
+    for snapshot in snapshots {
+        assert_snapshot!(snapshot);
+    }
+}
+
+#[test]
+pub fn move_focus_is_disabled_in_fullscreen() {
+    let fake_win_size = Winsize {
+        ws_col: 121,
+        ws_row: 20,
+        ws_xpixel: 0,
+        ws_ypixel: 0
+    };
+    let mut fake_input_output = get_fake_os_input(&fake_win_size);
+    fake_input_output.add_terminal_input(&[
+        SPLIT_VERTICALLY,
+        TOGGLE_ACTIVE_TERMINAL_FULLSCREEN,
+        MOVE_FOCUS,
+        TOGGLE_ACTIVE_TERMINAL_FULLSCREEN,
         QUIT,
     ]);
     start(Box::new(fake_input_output.clone()), Opt::default());
