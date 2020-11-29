4f625c557aa8dc33564b1565c9cac28e400c4a9b
Aram Drevekenin <aram@poor.dev>
2020-11-29 19:06:07 +0100

    fix(tests): send proper data

--- src/tests/integration/compatibility.rs
+++ src/tests/integration/compatibility.rs
@@ -225,7 +225,7 @@ pub fn htop_scrolling() {
     };
     let fixture_name = "htop_scrolling";
     let mut fake_input_output = get_fake_os_input(&fake_win_size, fixture_name);
-    fake_input_output.add_terminal_input(&[COMMAND_TOGGLE, COMMAND_TOGGLE, QUIT]);
+    fake_input_output.add_terminal_input(&[&COMMAND_TOGGLE, &COMMAND_TOGGLE, &QUIT]);
     start(Box::new(fake_input_output.clone()), Opt::default());
     let output_frames = fake_input_output
         .stdout_writer
