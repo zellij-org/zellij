c47fed927e8fad0395266fb30c58a094e430bee0
Aram Drevekenin <aram@poor.dev>
2020-11-30 09:40:24 +0100

    fic(compatibility): htop scrolling issues (#77)
    * fix(compatibility): scrolling inside scroll region with 'M' esc dispatch
    
    * style(format): make rustfmt happy
    
    * fix(tests): send proper data
    
    * fix(logs): do not crash if log file doesn't exist

--- src/main.rs
+++ src/main.rs
@@ -450,10 +450,11 @@ pub fn start(mut os_input: Box<dyn OsApi>, opts: Opt) {
     }
     // cleanup();
     let reset_style = "\u{1b}[m";
+    let show_cursor = "\u{1b}[?25h";
     let goto_start_of_last_line = format!("\u{1b}[{};{}H", full_screen_ws.rows, 1);
     let goodbye_message = format!(
-        "{}\n{}Bye from Mosaic!",
-        goto_start_of_last_line, reset_style
+        "{}\n{}{}Bye from Mosaic!",
+        goto_start_of_last_line, reset_style, show_cursor
     );
 
     os_input.unset_raw_mode(0);
