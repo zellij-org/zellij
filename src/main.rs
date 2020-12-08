cb99090c94b13d1bbab3844f8a1bb8615845e6cd
Kunal Mohan <kunalmohan99@gmail.com>
2020-12-08 11:44:52 +0530

    Use os_input instead of println!()

--- src/main.rs
+++ src/main.rs
@@ -489,7 +489,11 @@ pub fn start(mut os_input: Box<dyn OsApi>, opts: Opt) {
                 let _ = send_pty_instructions.send((PtyInstruction::Quit, err_ctx));
                 os_input.unset_raw_mode(0);
                 let goto_start_of_last_line = format!("\u{1b}[{};{}H", full_screen_ws.rows, 1);
-                println!("{}\n{}", goto_start_of_last_line, backtrace);
+                let error = format!("{}\n{}", goto_start_of_last_line, backtrace);
+                let _ = os_input
+                    .get_stdout_writer()
+                    .write(error.as_bytes())
+                    .unwrap();
                 for thread_handler in active_threads {
                     let _ = thread_handler.join();
                 }
