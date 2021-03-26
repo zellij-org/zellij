98c4227c816bf2852aac7a638cf9f955451c5f35
a-kenji <aks.kenji@protonmail.com>
2021-03-26 21:59:20 +0100

    Fix tests

--- src/common/input/config.rs
+++ src/common/input/config.rs
@@ -128,16 +128,17 @@ mod config_test {
     use super::*;
 
     #[test]
-    fn no_config_file_equals_default_config() {
+    fn clean_option_equals_default_config() {
         let no_file = PathBuf::from(r"../fixtures/config/config.yamlll");
-        let config = Config::from_option_or_default(Some(no_file)).unwrap();
+        let cli_config = ConfigCli::Config { path: Some(no_file) ,clean: true};
+        let config = Config::from_cli_config(Some(cli_config)).unwrap();
         let default = Config::default();
         assert_eq!(config, default);
     }
 
     #[test]
     fn no_config_option_file_equals_default_config() {
-        let config = Config::from_option_or_default(None).unwrap();
+        let config = Config::from_cli_config(None).unwrap();
         let default = Config::default();
         assert_eq!(config, default);
     }
