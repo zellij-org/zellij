605393ad96e8c2afde5f30a117cbdbb29616f49d
Brooks J Rady <b.j.rady@gmail.com>
2021-06-03 13:29:18 +0100

    refactor(panes): move to parametric pane sizes
    This change introduces the `Fixed` and `Percent` dimensions for pane
    sizes, laying the groundwork for a parametric resize / layout system
    down the line.
    
    IMPORTANT: This commit is hillariously broken and doesn't compile.

--- zellij-utils/src/pane_size.rs
+++ zellij-utils/src/pane_size.rs
@@ -3,25 +3,29 @@ use serde::{Deserialize, Serialize};
 
 /// Contains the position and size of a [`Pane`], or more generally of any terminal, measured
 /// in character rows and columns.
-#[derive(Clone, Copy, Debug, Default, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
 pub struct PositionAndSize {
     pub x: usize,
     pub y: usize,
-    pub rows: usize,
-    pub cols: usize,
-    // FIXME: Honestly, these shouldn't exist and rows / columns should be enums like:
-    // Dimension::Flex(usize) / Dimension::Fixed(usize), but 400+ compiler errors is more than
-    // I'm in the mood for right now...
-    pub rows_fixed: bool,
-    pub cols_fixed: bool,
+    pub rows: Dimension,
+    pub cols: Dimension,
+}
+
+#[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+pub enum Dimension {
+    /// Constrains the dimension to a fixed, integer number of rows / columns
+    Fixed(usize),
+    /// Constrains the dimension to a flexible percent size of the total screen
+    Percent(u8),
 }
 
 impl From<Winsize> for PositionAndSize {
     fn from(winsize: Winsize) -> PositionAndSize {
         PositionAndSize {
-            cols: winsize.ws_col as usize,
-            rows: winsize.ws_row as usize,
-            ..Default::default()
+            x: 0,
+            y: 0,
+            cols: Dimension::Fixed(winsize.ws_col as usize),
+            rows: Dimension::Fixed(winsize.ws_row as usize),
         }
     }
 }
