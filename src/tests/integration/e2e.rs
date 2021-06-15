89f8932b23810b01b5a00879889d4e9c8f45b227
Aram Drevekenin <aram@poor.dev>
2021-06-15 13:37:15 +0200

    chore(build): github action

--- src/tests/integration/e2e.rs
+++ src/tests/integration/e2e.rs
@@ -25,7 +25,7 @@ use ssh2::Session;
 use std::io::prelude::*;
 
 const ZELLIJ_EXECUTABLE_LOCATION: &str = "/usr/src/zellij/zellij";
-const CONNECTION_STRING: &str = "localhost:1337";
+const CONNECTION_STRING: &str = "localhost:2222";
 const CONNECTION_USERNAME: &str = "test";
 const CONNECTION_PASSWORD: &str = "test";
 
