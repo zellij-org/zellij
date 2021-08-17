da6b747c5ec96c70c96a1530d50b88a141220014
Tw <wei.tan@intel.com>
2021-08-17 14:08:46 +0800

    feat(color): add unit test for monochrome mode
    Signed-off-by: Tw <wei.tan@intel.com>

--- zellij-server/src/panes/unit/terminal_pane_tests.rs
+++ zellij-server/src/panes/unit/terminal_pane_tests.rs
@@ -1,6 +1,7 @@
 use super::super::TerminalPane;
 use crate::tab::Pane;
-use ::insta::assert_snapshot;
+use ansi_term::Color::{Fixed, RGB};
+use insta::assert_snapshot;
 use zellij_utils::pane_size::PositionAndSize;
 use zellij_utils::zellij_tile::data::Palette;
 
@@ -14,8 +15,8 @@ pub fn scrolling_inside_a_pane() {
         ..Default::default()
     };
     let pid = 1;
-    let palette = Palette::default();
-    let mut terminal_pane = TerminalPane::new(pid, fake_win_size, Some(palette), 0); // 0 is the pane index
+    let palette = Some(Palette::default());
+    let mut terminal_pane = TerminalPane::new(pid, fake_win_size, palette, 0); // 0 is the pane index
     let mut text_to_fill_pane = String::new();
     for i in 0..30 {
         text_to_fill_pane.push_str(&format!("\rline {}\n", i + 1));
@@ -27,4 +28,26 @@ pub fn scrolling_inside_a_pane() {
     assert_snapshot!(format!("{:?}", terminal_pane.grid));
     terminal_pane.clear_scroll();
     assert_snapshot!(format!("{:?}", terminal_pane.grid));
+}
+
+#[test]
+pub fn monochrome_pane() {
+    let fake_win_size = PositionAndSize {
+        cols: 10,
+        rows: 1,
+        x: 0,
+        y: 0,
+        ..Default::default()
+    };
+    let pid = 1;
+    let palette = None;
+    let mut terminal_pane = TerminalPane::new(pid, fake_win_size, palette, 0);
+    let text_style = ansi_term::Style::new()
+        .fg(Fixed(77))
+        .on(RGB(7, 7, 7))
+        .bold(); // styles other than colors should not be stripped
+    let text_to_fill_pane = format!("{}", text_style.paint("Hi Zellij"));
+
+    terminal_pane.handle_pty_bytes(dbg!(text_to_fill_pane).as_bytes().to_vec());
+    assert_snapshot!(format!("{:?}", terminal_pane.render().unwrap()));
 }
