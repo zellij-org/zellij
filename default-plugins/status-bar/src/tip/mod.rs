4aa674ef3ad6d3ea97b6836c6920cab68a8eeda3
jaeheonji <atx6419@gmail.com>
2021-12-01 21:28:13 +0900

    chore(status-bar): update missing fixes

--- default-plugins/status-bar/src/tip/mod.rs
+++ default-plugins/status-bar/src/tip/mod.rs
@@ -14,17 +14,3 @@ pub struct TipBody {
     pub medium: TipFn,
     pub full: TipFn,
 }
-
-#[macro_export]
-macro_rules! strings {
-    ($ANSIStrings:expr) => {{
-        let strings: &[ANSIString<'static>] = $ANSIStrings;
-
-        let ansi_strings = ANSIStrings(strings);
-
-        LinePart {
-            part: format!("{}", ansi_strings),
-            len: unstyled_len(&ansi_strings),
-        }
-    }};
-}
