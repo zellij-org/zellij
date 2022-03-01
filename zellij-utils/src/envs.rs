6d653e1521084d633367c6f4d8afc092cb30c17b
a-kenji <aks.kenji@protonmail.com>
2022-03-01 15:47:52 +0100

    add: set `env` var's from config and layout (#1154)
    Add ability to set `ENVIRONMENT VARIABLES` from the
    config and the layout files.
    
    example:
    
    ```
    env:
      ZELLIJ_CONFIG:
        DEFAULT
    ```
    
    or
    
    ```
    env:
      ZELLIJ_LAYOUT_NAME:
        BUILD_SESSION
    ```
    
    If two keys conflict (configuration and layout), then the key
    from the layout is used.
    
    fixes: #1059

--- zellij-utils/src/envs.rs
+++ zellij-utils/src/envs.rs
@@ -1,6 +1,10 @@
 /// Uniformly operates ZELLIJ* environment variables
 use anyhow::Result;
-use std::env::{set_var, var};
+use serde::{Deserialize, Serialize};
+use std::{
+    collections::HashMap,
+    env::{set_var, var},
+};
 
 pub const ZELLIJ_ENV_KEY: &str = "ZELLIJ";
 pub fn get_zellij() -> Result<String> {
@@ -23,4 +27,27 @@ pub fn set_session_name(v: String) {
 pub const SOCKET_DIR_ENV_KEY: &str = "ZELLIJ_SOCKET_DIR";
 pub fn get_socket_dir() -> Result<String> {
     Ok(var(SOCKET_DIR_ENV_KEY)?)
+}
+
+/// Manage ENVIRONMENT VARIABLES from the configuration and the layout files
+#[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
+pub struct EnvironmentVariablesFromYaml {
+    env: HashMap<String, String>,
+}
+
+impl EnvironmentVariablesFromYaml {
+    /// Merges two structs, keys from `other` supercede keys from `self`
+    pub fn merge(&self, other: Self) -> Self {
+        let mut env = self.clone();
+        env.env.extend(other.env);
+        env
+    }
+
+    /// Set all the ENVIRONMENT VARIABLES, that are configured
+    /// in the configuration and layout files
+    pub fn set_vars(&self) {
+        for (k, v) in &self.env {
+            set_var(k, v);
+        }
+    }
 }
