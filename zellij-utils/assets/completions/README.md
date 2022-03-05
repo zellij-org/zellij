b2287c9a233f837c64f2eb7aaf3dcb74ab98495d
a-kenji <aks.kenji@protonmail.com>
2022-03-05 15:55:23 +0100

    add(comp): dynamic completions for fish
    And infrastructure to make it possible
    to add more dynamic completions for
    different shells in the future.
    
    eg:
    
    ```
    zellij attach [completes-active-sessions]
    zellij kill-session [completes-active-sessions]
    ```
    
    fixes: #1030

---
+++ zellij-utils/assets/completions/README.md
@@ -0,0 +1,2 @@
+Extra completion files that get appended to the
+clap output, in order to support dynamic commands.
---
+++ zellij-utils/assets/completions/comp.fish
@@ -0,0 +1,8 @@
+function __fish_complete_sessions
+    zellij list-sessions 2>/dev/null
+end
+complete -c zellij -n "__fish_seen_subcommand_from attach" -f -a "(__fish_complete_sessions)" -d "Session"
+complete -c zellij -n "__fish_seen_subcommand_from a" -f -a "(__fish_complete_sessions)" -d "Session"
+complete -c zellij -n "__fish_seen_subcommand_from kill-session" -f -a "(__fish_complete_sessions)" -d "Session"
+complete -c zellij -n "__fish_seen_subcommand_from a" -f -a "(__fish_complete_sessions)" -d "Session"
+complete -c zellij -n "__fish_seen_subcommand_from setup --generate-completion" -f -a "bash elvish fish zsh powershell" -d "Shell"
