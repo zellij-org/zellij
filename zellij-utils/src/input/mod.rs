09c67069f3c36a9824bde078e798e5b14a642b22
Andreas Hartmann <hartan@7x.de>
2022-06-27 12:33:07 +0200

    utils: Share more modules with wasm
    that work on both wasm and unix natively. This requires factoring out
    bits of code in the `setup` and `input` modules into a private submodule
    that is re-exported when the compilation target is *not* "wasm". The
    following modules are now available to the wasm target:
    
    - cli
    - consts
    - data
    - envs
    - input (partial)
        - actions
        - command
        - configs
        - keybinds
        - layout
        - options
        - plugins
        - theme
    - pane_size
    - position
    - setup (partial)
    - shared
    
    The remaining modules unavailable to wasm have dependencies on crates
    that cannot compile against wasm, such as `async_std` or `termwiz`.

--- zellij-utils/src/input/mod.rs
+++ zellij-utils/src/input/mod.rs
@@ -1,21 +1,21 @@
 //! The way terminal input is handled.
-
 pub mod actions;
 pub mod command;
 pub mod config;
 pub mod keybinds;
 pub mod layout;
-pub mod mouse;
 pub mod options;
 pub mod plugins;
 pub mod theme;
 
-use super::{
-    data::{CharOrArrow, Direction, Style},
-    data::{InputMode, Key, ModeInfo, PluginCapabilities},
+// Can't use this in wasm due to dependency on the `termwiz` crate.
+#[cfg(not(target_family = "wasm"))]
+pub mod mouse;
+
+use crate::{
+    envs,
+    data::{InputMode, Key, ModeInfo, PluginCapabilities, Style},
 };
-use crate::envs;
-use termwiz::input::{InputEvent, InputParser, KeyCode, KeyEvent, Modifiers};
 
 /// Creates a [`ModeInfo`] struct indicating the current [`InputMode`] and its keybinds
 /// (as pairs of [`String`]s).
@@ -98,79 +98,89 @@ pub fn get_mode_info(mode: InputMode, style: Style, capabilities: PluginCapabili
     }
 }
 
-pub fn parse_keys(input_bytes: &[u8]) -> Vec<Key> {
-    let mut ret = vec![];
-    let mut input_parser = InputParser::new(); // this is the termwiz InputParser
-    let maybe_more = false;
-    let parse_input_event = |input_event: InputEvent| {
-        if let InputEvent::Key(key_event) = input_event {
-            ret.push(cast_termwiz_key(key_event, input_bytes));
+#[cfg(not(target_family = "wasm"))]
+pub use not_wasm::*;
+
+#[cfg(not(target_family = "wasm"))]
+mod not_wasm {
+    use super::*;
+    use crate::data::{CharOrArrow, Direction};
+    use termwiz::input::{InputEvent, InputParser, KeyCode, KeyEvent, Modifiers};
+
+    pub fn parse_keys(input_bytes: &[u8]) -> Vec<Key> {
+        let mut ret = vec![];
+        let mut input_parser = InputParser::new(); // this is the termwiz InputParser
+        let maybe_more = false;
+        let parse_input_event = |input_event: InputEvent| {
+            if let InputEvent::Key(key_event) = input_event {
+                ret.push(cast_termwiz_key(key_event, input_bytes));
+            }
+        };
+        input_parser.parse(input_bytes, parse_input_event, maybe_more);
+        ret
+    }
+
+    // FIXME: This is an absolutely cursed function that should be destroyed as soon
+    // as an alternative that doesn't touch zellij-tile can be developed...
+    pub fn cast_termwiz_key(event: KeyEvent, raw_bytes: &[u8]) -> Key {
+        let modifiers = event.modifiers;
+
+        // *** THIS IS WHERE WE SHOULD WORK AROUND ISSUES WITH TERMWIZ ***
+        if raw_bytes == [8] {
+            return Key::Ctrl('h');
+        };
+
+        match event.key {
+            KeyCode::Char(c) => {
+                if modifiers.contains(Modifiers::CTRL) {
+                    Key::Ctrl(c.to_lowercase().next().unwrap_or_default())
+                } else if modifiers.contains(Modifiers::ALT) {
+                    Key::Alt(CharOrArrow::Char(c))
+                } else {
+                    Key::Char(c)
+                }
+            },
+            KeyCode::Backspace => Key::Backspace,
+            KeyCode::LeftArrow | KeyCode::ApplicationLeftArrow => {
+                if modifiers.contains(Modifiers::ALT) {
+                    Key::Alt(CharOrArrow::Direction(Direction::Left))
+                } else {
+                    Key::Left
+                }
+            },
+            KeyCode::RightArrow | KeyCode::ApplicationRightArrow => {
+                if modifiers.contains(Modifiers::ALT) {
+                    Key::Alt(CharOrArrow::Direction(Direction::Right))
+                } else {
+                    Key::Right
+                }
+            },
+            KeyCode::UpArrow | KeyCode::ApplicationUpArrow => {
+                if modifiers.contains(Modifiers::ALT) {
+                    //Key::AltPlusUpArrow
+                    Key::Alt(CharOrArrow::Direction(Direction::Up))
+                } else {
+                    Key::Up
+                }
+            },
+            KeyCode::DownArrow | KeyCode::ApplicationDownArrow => {
+                if modifiers.contains(Modifiers::ALT) {
+                    Key::Alt(CharOrArrow::Direction(Direction::Down))
+                } else {
+                    Key::Down
+                }
+            },
+            KeyCode::Home => Key::Home,
+            KeyCode::End => Key::End,
+            KeyCode::PageUp => Key::PageUp,
+            KeyCode::PageDown => Key::PageDown,
+            KeyCode::Tab => Key::BackTab, // TODO: ???
+            KeyCode::Delete => Key::Delete,
+            KeyCode::Insert => Key::Insert,
+            KeyCode::Function(n) => Key::F(n),
+            KeyCode::Escape => Key::Esc,
+            KeyCode::Enter => Key::Char('\n'),
+            _ => Key::Esc, // there are other keys we can implement here, but we might need additional terminal support to implement them, not just exhausting this enum
         }
-    };
-    input_parser.parse(input_bytes, parse_input_event, maybe_more);
-    ret
-}
-
-// FIXME: This is an absolutely cursed function that should be destroyed as soon
-// as an alternative that doesn't touch zellij-tile can be developed...
-pub fn cast_termwiz_key(event: KeyEvent, raw_bytes: &[u8]) -> Key {
-    let modifiers = event.modifiers;
-
-    // *** THIS IS WHERE WE SHOULD WORK AROUND ISSUES WITH TERMWIZ ***
-    if raw_bytes == [8] {
-        return Key::Ctrl('h');
-    };
-
-    match event.key {
-        KeyCode::Char(c) => {
-            if modifiers.contains(Modifiers::CTRL) {
-                Key::Ctrl(c.to_lowercase().next().unwrap_or_default())
-            } else if modifiers.contains(Modifiers::ALT) {
-                Key::Alt(CharOrArrow::Char(c))
-            } else {
-                Key::Char(c)
-            }
-        },
-        KeyCode::Backspace => Key::Backspace,
-        KeyCode::LeftArrow | KeyCode::ApplicationLeftArrow => {
-            if modifiers.contains(Modifiers::ALT) {
-                Key::Alt(CharOrArrow::Direction(Direction::Left))
-            } else {
-                Key::Left
-            }
-        },
-        KeyCode::RightArrow | KeyCode::ApplicationRightArrow => {
-            if modifiers.contains(Modifiers::ALT) {
-                Key::Alt(CharOrArrow::Direction(Direction::Right))
-            } else {
-                Key::Right
-            }
-        },
-        KeyCode::UpArrow | KeyCode::ApplicationUpArrow => {
-            if modifiers.contains(Modifiers::ALT) {
-                //Key::AltPlusUpArrow
-                Key::Alt(CharOrArrow::Direction(Direction::Up))
-            } else {
-                Key::Up
-            }
-        },
-        KeyCode::DownArrow | KeyCode::ApplicationDownArrow => {
-            if modifiers.contains(Modifiers::ALT) {
-                Key::Alt(CharOrArrow::Direction(Direction::Down))
-            } else {
-                Key::Down
-            }
-        },
-        KeyCode::Home => Key::Home,
-        KeyCode::End => Key::End,
-        KeyCode::PageUp => Key::PageUp,
-        KeyCode::PageDown => Key::PageDown,
-        KeyCode::Tab => Key::BackTab, // TODO: ???
-        KeyCode::Delete => Key::Delete,
-        KeyCode::Insert => Key::Insert,
-        KeyCode::Function(n) => Key::F(n),
-        KeyCode::Escape => Key::Esc,
-        KeyCode::Enter => Key::Char('\n'),
-        _ => Key::Esc, // there are other keys we can implement here, but we might need additional terminal support to implement them, not just exhausting this enum
     }
 }
