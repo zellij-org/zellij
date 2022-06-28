a770f80d5a7f855726cc2912177b3e79fbf80f83
Andreas Hartmann <hartan@7x.de>
2022-06-28 15:58:18 +0200

    status-bar/second_line: Display configured binds
    Instead of showing some hard-coded default values. For each mode, reads
    the keybindings from the configured keybindings based on some sequence
    of action. For example, the keybinding for `New` in the `Pane` menu is
    now determined by looking into the configured keybindings and finding
    what key is bound to the `Action::NewPane(None)` action.
    
    If no keybinding is found for a given sequence of actions, it will not
    show up in the segments either.

--- default-plugins/status-bar/src/second_line.rs
+++ default-plugins/status-bar/src/second_line.rs
@@ -1,8 +1,11 @@
+use super::{action_key, to_normal};
 use ansi_term::{
     ANSIStrings,
     Color::{Fixed, RGB},
     Style,
 };
+use std::collections::VecDeque;
+use zellij_tile::prelude::actions::Action;
 use zellij_tile::prelude::*;
 use zellij_tile_utils::palette_match;
 
@@ -26,31 +29,42 @@ enum StatusBarTextBoldness {
 
 fn full_length_shortcut(
     is_first_shortcut: bool,
-    letter: &str,
-    description: &str,
+    key: Vec<Key>,
+    action: &str,
     palette: Palette,
 ) -> LinePart {
     let text_color = palette_match!(match palette.theme_hue {
         ThemeHue::Dark => palette.white,
         ThemeHue::Light => palette.black,
     });
+    let key = key
+        .iter()
+        .map(|key| format!("{}", key))
+        .collect::<Vec<String>>()
+        .join("");
+    if key.is_empty() {
+        return LinePart {
+            part: "".to_string(),
+            len: 0,
+        };
+    }
     let green_color = palette_match!(palette.green);
     let separator = if is_first_shortcut { " " } else { " / " };
     let separator = Style::new().fg(text_color).paint(separator);
-    let shortcut_len = letter.chars().count() + 3; // 2 for <>'s around shortcut, 1 for the space
+    let shortcut_len = key.chars().count() + 3; // 2 for <>'s around shortcut, 1 for the space
     let shortcut_left_separator = Style::new().fg(text_color).paint("<");
-    let shortcut = Style::new().fg(green_color).bold().paint(letter);
+    let shortcut = Style::new().fg(green_color).bold().paint(key);
     let shortcut_right_separator = Style::new().fg(text_color).paint("> ");
-    let description_len = description.chars().count();
-    let description = Style::new().fg(text_color).bold().paint(description);
-    let len = shortcut_len + description_len + separator.chars().count();
+    let action_len = action.chars().count();
+    let action = Style::new().fg(text_color).bold().paint(action);
+    let len = shortcut_len + action_len + separator.chars().count();
     LinePart {
         part: ANSIStrings(&[
             separator,
             shortcut_left_separator,
             shortcut,
             shortcut_right_separator,
-            description,
+            action,
         ])
         .to_string(),
         len,
@@ -59,14 +73,16 @@ fn full_length_shortcut(
 
 fn first_word_shortcut(
     is_first_shortcut: bool,
-    letter: &str,
-    description: &str,
+    key: &Key,
+    _action: &[Action],
     palette: Palette,
 ) -> LinePart {
     let text_color = palette_match!(match palette.theme_hue {
         ThemeHue::Dark => palette.white,
         ThemeHue::Light => palette.black,
     });
+    let letter = format!("{}", key);
+    let description = "test".to_string();
     let green_color = palette_match!(palette.green);
     let separator = if is_first_shortcut { " " } else { " / " };
     let separator = Style::new().fg(text_color).paint(separator);
@@ -173,20 +189,172 @@ fn select_pane_shortcut(palette: Palette) -> LinePart {
     show_extra_hints(palette, text_with_style.to_vec())
 }
 
+fn add_shortcut(help: &ModeInfo, mut linepart: LinePart, text: &str, keys: Vec<Key>) -> LinePart {
+    let shortcut = full_length_shortcut(false, keys, text, help.style.colors);
+    linepart.len += shortcut.len;
+    linepart.part = format!("{}{}", linepart.part, shortcut);
+    linepart
+}
+
 fn full_shortcut_list_nonstandard_mode(
     extra_hint_producing_function: fn(Palette) -> LinePart,
 ) -> impl FnOnce(&ModeInfo) -> LinePart {
     move |help| {
-        let mut line_part = LinePart::default();
-        for (i, (letter, description)) in help.keybinds.iter().enumerate() {
-            let shortcut = full_length_shortcut(i == 0, letter, description, help.style.colors);
-            line_part.len += shortcut.len;
-            line_part.part = format!("{}{}", line_part.part, shortcut,);
+        let km: VecDeque<(Key, Vec<Action>)> = help.keybinds.clone().into();
+        let mut lp = LinePart::default();
+
+        if help.mode == InputMode::Pane || help.mode == InputMode::Tab {
+            // Shared keybindings
+            lp = add_shortcut(
+                help,
+                lp,
+                "Move Focus",
+                action_key!(km, Action::MoveFocus(_)),
+            );
+            lp = add_shortcut(help, lp, "New", action_key!(km, Action::NewPane(None)));
+            lp = add_shortcut(
+                help,
+                lp,
+                "New",
+                action_key!(km, Action::NewTab(_), to_normal!()),
+            );
+            lp = add_shortcut(
+                help,
+                lp,
+                "Close",
+                action_key!(km, Action::CloseFocus, to_normal!()),
+            );
+            lp = add_shortcut(
+                help,
+                lp,
+                "Rename",
+                action_key!(
+                    km,
+                    Action::SwitchToMode(InputMode::RenamePane),
+                    Action::PaneNameInput(_)
+                ),
+            );
+            lp = add_shortcut(
+                help,
+                lp,
+                "Rename",
+                action_key!(
+                    km,
+                    Action::SwitchToMode(InputMode::RenameTab),
+                    Action::TabNameInput(_)
+                ),
+            );
         }
+
+        // Pane keybindings
+        lp = add_shortcut(
+            help,
+            lp,
+            "Split Down",
+            action_key!(
+                km,
+                Action::NewPane(Some(actions::Direction::Down)),
+                to_normal!()
+            ),
+        );
+        lp = add_shortcut(
+            help,
+            lp,
+            "Split Right",
+            action_key!(
+                km,
+                Action::NewPane(Some(actions::Direction::Right)),
+                to_normal!()
+            ),
+        );
+        lp = add_shortcut(
+            help,
+            lp,
+            "Fullscreen",
+            action_key!(km, Action::ToggleFocusFullscreen, to_normal!()),
+        );
+        lp = add_shortcut(
+            help,
+            lp,
+            "Frames",
+            action_key!(km, Action::TogglePaneFrames, to_normal!()),
+        );
+        lp = add_shortcut(
+            help,
+            lp,
+            "Floating Toggle",
+            action_key!(km, Action::ToggleFloatingPanes, to_normal!()),
+        );
+        lp = add_shortcut(
+            help,
+            lp,
+            "Embed Pane",
+            action_key!(km, Action::TogglePaneEmbedOrFloating, to_normal!()),
+        );
+        lp = add_shortcut(help, lp, "Next", action_key!(km, Action::SwitchFocus));
+
+        // Tab keybindings
+        lp = add_shortcut(
+            help,
+            lp,
+            "Sync",
+            action_key!(km, Action::ToggleActiveSyncTab, to_normal!()),
+        );
+        lp = add_shortcut(help, lp, "Toggle", action_key!(km, Action::ToggleTab));
+
+        // Resize keybindings
+        // By default these are defined in every mode
+        // Arrow keys
+        if help.mode == InputMode::Resize {
+            let arrow_keys = action_key!(km, Action::Resize(actions::ResizeDirection::Left))
+                .into_iter()
+                .chain(action_key!(km, Action::Resize(actions::ResizeDirection::Down)).into_iter())
+                .chain(action_key!(km, Action::Resize(actions::ResizeDirection::Up)).into_iter())
+                .chain(action_key!(km, Action::Resize(actions::ResizeDirection::Right)).into_iter())
+                .collect();
+            lp = add_shortcut(help, lp, "Resize", arrow_keys);
+
+            let pme = action_key!(km, Action::Resize(actions::ResizeDirection::Increase))
+                .into_iter()
+                .chain(
+                    action_key!(km, Action::Resize(actions::ResizeDirection::Decrease)).into_iter(),
+                )
+                .collect();
+            lp = add_shortcut(help, lp, "Increase/Decrease Size", pme);
+        }
+
+        // Move keybindings
+        lp = add_shortcut(help, lp, "Move", action_key!(km, Action::MovePane(Some(_))));
+        lp = add_shortcut(
+            help,
+            lp,
+            "Next Pane",
+            action_key!(km, Action::MovePane(None)),
+        );
+
+        // Scroll keybindings
+        // arrows - Scroll
+        // Pg - Scroll Page
+        // ud - Scroll Half Page
+        lp = add_shortcut(
+            help,
+            lp,
+            "Edit Scrollback in Default Editor",
+            action_key!(km, Action::EditScrollback, to_normal!()),
+        );
+
+        // Session keybindings
+        lp = add_shortcut(help, lp, "Detach", action_key!(km, Action::Detach));
+
+        //for (i, (key, action)) in help.keybinds.iter().enumerate() {
+        //    let shortcut = full_length_shortcut(i == 0, key, action, help.style.colors);
+        //    lp.len += shortcut.len;
+        //    lp.part = format!("{}{}", line_part.part, shortcut,);
+        //}
         let select_pane_shortcut = extra_hint_producing_function(help.style.colors);
-        line_part.len += select_pane_shortcut.len;
-        line_part.part = format!("{}{}", line_part.part, select_pane_shortcut,);
-        line_part
+        lp.len += select_pane_shortcut.len;
+        lp.part = format!("{}{}", lp.part, select_pane_shortcut,);
+        lp
     }
 }
 
@@ -463,19 +631,13 @@ pub fn full_tmux_mode_indication(help: &ModeInfo) -> LinePart {
     let shortcut_right_separator = Style::new().fg(white_color).bold().paint("): ");
     let tmux_mode_text = "TMUX MODE";
     let tmux_mode_indicator = Style::new().fg(orange_color).bold().paint(tmux_mode_text);
-    let mut line_part = LinePart {
+    let line_part = LinePart {
         part: format!(
             "{}{}{}",
             shortcut_left_separator, tmux_mode_indicator, shortcut_right_separator
         ),
         len: tmux_mode_text.chars().count() + 5, // 2 for the separators, 3 for the colon and following space
     };
-
-    for (i, (letter, description)) in help.keybinds.iter().enumerate() {
-        let shortcut = full_length_shortcut(i == 0, letter, description, help.style.colors);
-        line_part.len += shortcut.len;
-        line_part.part = format!("{}{}", line_part.part, shortcut,);
-    }
     line_part
 }
 
