3b756332da1e598c52fca180b70aec0bbedc7396
Andreas Hartmann <hartan@7x.de>
2022-06-30 10:54:44 +0200

    status-bar/second-line: Refactor key hints
    Instead of determining the appropriate key hints for every case
    separately (i.e. enough space to show all, show shortened, shot
    best-effort), create a central function that returns for the current
    `InputMode` a Vector with tuples of:
    
    - A String to show in full-length mode
    - A String to show in shortened/best-effort mode
    - The vector of keys that goes with this key hint
    
    This allows all functions that need the hints to iterate over the vector
    and pick whatever hint suits them along with the Keys to display.

--- default-plugins/status-bar/src/second_line.rs
+++ default-plugins/status-bar/src/second_line.rs
@@ -4,7 +4,6 @@ use ansi_term::{
     Color::{Fixed, RGB},
     Style,
 };
-use std::collections::VecDeque;
 use zellij_tile::prelude::actions::Action;
 use zellij_tile::prelude::*;
 use zellij_tile_utils::palette_match;
@@ -205,162 +204,114 @@ fn full_shortcut_list_nonstandard_mode(
     extra_hint_producing_function: fn(Palette) -> LinePart,
 ) -> impl FnOnce(&ModeInfo) -> LinePart {
     move |help| {
-        let km: VecDeque<(Key, Vec<Action>)> = help.keybinds.clone().into();
         let mut lp = LinePart::default();
+        let keys_and_hints = get_keys_and_hints(help);
 
-        if help.mode == InputMode::Pane || help.mode == InputMode::Tab {
-            // Shared keybindings
-            lp = add_shortcut(
-                help,
-                lp,
-                "Move Focus",
-                action_key!(km, Action::MoveFocus(_)),
-            );
-            lp = add_shortcut(help, lp, "New", action_key!(km, Action::NewPane(None)));
-            lp = add_shortcut(
-                help,
-                lp,
-                "New",
-                action_key!(km, Action::NewTab(_), to_normal!()),
-            );
-            lp = add_shortcut(
-                help,
-                lp,
-                "Close",
-                action_key!(km, Action::CloseFocus, to_normal!()),
-            );
-            lp = add_shortcut(
-                help,
-                lp,
-                "Rename",
-                action_key!(
-                    km,
-                    Action::SwitchToMode(InputMode::RenamePane),
-                    Action::PaneNameInput(_)
-                ),
-            );
-            lp = add_shortcut(
-                help,
-                lp,
-                "Rename",
-                action_key!(
-                    km,
-                    Action::SwitchToMode(InputMode::RenameTab),
-                    Action::TabNameInput(_)
-                ),
-            );
+        for (long, _short, keys) in keys_and_hints.into_iter() {
+            lp = add_shortcut(help, lp, &long, keys.to_vec());
         }
 
-        // Pane keybindings
-        lp = add_shortcut(
-            help,
-            lp,
-            "Split Down",
-            action_key!(
-                km,
-                Action::NewPane(Some(actions::Direction::Down)),
-                to_normal!()
-            ),
-        );
-        lp = add_shortcut(
-            help,
-            lp,
-            "Split Right",
-            action_key!(
-                km,
-                Action::NewPane(Some(actions::Direction::Right)),
-                to_normal!()
-            ),
-        );
-        lp = add_shortcut(
-            help,
-            lp,
-            "Fullscreen",
-            action_key!(km, Action::ToggleFocusFullscreen, to_normal!()),
-        );
-        lp = add_shortcut(
-            help,
-            lp,
-            "Frames",
-            action_key!(km, Action::TogglePaneFrames, to_normal!()),
-        );
-        lp = add_shortcut(
-            help,
-            lp,
-            "Floating Toggle",
-            action_key!(km, Action::ToggleFloatingPanes, to_normal!()),
-        );
-        lp = add_shortcut(
-            help,
-            lp,
-            "Embed Pane",
-            action_key!(km, Action::TogglePaneEmbedOrFloating, to_normal!()),
-        );
-        lp = add_shortcut(help, lp, "Next", action_key!(km, Action::SwitchFocus));
-
-        // Tab keybindings
-        lp = add_shortcut(
-            help,
-            lp,
-            "Sync",
-            action_key!(km, Action::ToggleActiveSyncTab, to_normal!()),
-        );
-        lp = add_shortcut(help, lp, "Toggle", action_key!(km, Action::ToggleTab));
-
-        // Resize keybindings
-        // By default these are defined in every mode
-        // Arrow keys
-        if help.mode == InputMode::Resize {
-            let arrow_keys = action_key!(km, Action::Resize(actions::ResizeDirection::Left))
-                .into_iter()
-                .chain(action_key!(km, Action::Resize(actions::ResizeDirection::Down)).into_iter())
-                .chain(action_key!(km, Action::Resize(actions::ResizeDirection::Up)).into_iter())
-                .chain(action_key!(km, Action::Resize(actions::ResizeDirection::Right)).into_iter())
-                .collect();
-            lp = add_shortcut(help, lp, "Resize", arrow_keys);
-
-            let pme = action_key!(km, Action::Resize(actions::ResizeDirection::Increase))
-                .into_iter()
-                .chain(
-                    action_key!(km, Action::Resize(actions::ResizeDirection::Decrease)).into_iter(),
-                )
-                .collect();
-            lp = add_shortcut(help, lp, "Increase/Decrease Size", pme);
-        }
-
-        // Move keybindings
-        lp = add_shortcut(help, lp, "Move", action_key!(km, Action::MovePane(Some(_))));
-        lp = add_shortcut(
-            help,
-            lp,
-            "Next Pane",
-            action_key!(km, Action::MovePane(None)),
-        );
-
-        // Scroll keybindings
-        // arrows - Scroll
-        // Pg - Scroll Page
-        // ud - Scroll Half Page
-        lp = add_shortcut(
-            help,
-            lp,
-            "Edit Scrollback in Default Editor",
-            action_key!(km, Action::EditScrollback, to_normal!()),
-        );
-
-        // Session keybindings
-        lp = add_shortcut(help, lp, "Detach", action_key!(km, Action::Detach));
-
-        //for (i, (key, action)) in help.keybinds.iter().enumerate() {
-        //    let shortcut = full_length_shortcut(i == 0, key, action, help.style.colors);
-        //    lp.len += shortcut.len;
-        //    lp.part = format!("{}{}", line_part.part, shortcut,);
-        //}
         let select_pane_shortcut = extra_hint_producing_function(help.style.colors);
         lp.len += select_pane_shortcut.len;
         lp.part = format!("{}{}", lp.part, select_pane_shortcut,);
         lp
     }
+}
+
+/// Collect all relevant keybindings and hints to display.
+///
+/// Creates a vector with tuples containing the following entries:
+///
+/// - A String to display for this keybinding when there are no size restrictions,
+/// - A shortened String (where sensible) to display if the whole second line becomes too long,
+/// - A `Vec<Key>` of the keys that map to this keyhint
+///
+/// This vector is created by iterating over the keybindings for the current [`InputMode`] and
+/// storing all Keybindings that match pre-defined patterns of `Action`s. For example, the
+/// `InputMode::Pane` input mode determines which keys to display for the "Move focus" hint by
+/// searching the keybindings for anything that matches the `Action::MoveFocus(_)` action. Since by
+/// default multiple keybindings map to some action patterns (e.g. `Action::MoveFocus(_)` is bound
+/// to "hjkl", the arrow keys and "Alt + <hjkl>"), we deduplicate the vector of all keybindings
+/// before processing it.
+///
+/// Therefore we sort it by the [`Key`]s of the current keymap and deduplicate the resulting sorted
+/// vector by the `Vec<Action>` action vectors bound to the keys. As such, when multiple keys map
+/// to the same sequence of actions, the keys that appear first in the [`Key`] structure will be
+/// displayed.
+// Please don't let rustfmt play with the formatting. It will stretch out the function to about
+// three times the length and all the keybinding vectors we generate become virtually unreadable
+// for humans.
+#[rustfmt::skip]
+fn get_keys_and_hints(mi: &ModeInfo) -> Vec<(String, String, Vec<Key>)> {
+    use Action as A;
+    use InputMode as IM;
+    use actions::Direction as Dir;
+    use actions::ResizeDirection as RDir;
+
+    let mut old_keymap = mi.keybinds.clone();
+    let s = |string: &str| string.to_string();
+
+    // Sort and deduplicate the keybindings first. We sort after the `Key`s, and deduplicate by
+    // their `Action` vectors. An unstable sort is fine here because if the user maps anything to
+    // the same key again, anything will happen...
+    old_keymap.sort_unstable_by(|(keya, _), (keyb, _)| keya.partial_cmp(keyb).unwrap());
+
+    let mut known_actions: Vec<Vec<Action>> = vec![];
+    let mut km = vec![];
+    for (key, acvec) in old_keymap.into_iter() {
+        if known_actions.contains(&acvec) {
+            // This action is known already
+            continue;
+        } else {
+            known_actions.push(acvec.to_vec());
+            km.push((key, acvec));
+        }
+    }
+
+    return if mi.mode == IM::Pane { vec![
+        (s("Move focus"), s("Move"), action_key!(km, A::MoveFocus(_))),
+        (s("New"), s("New"), action_key!(km, A::NewPane(None), to_normal!())),
+        (s("Close"), s("Close"), action_key!(km, A::CloseFocus, to_normal!())),
+        (s("Rename"), s("Rename"), action_key!(km, A::SwitchToMode(IM::RenamePane), A::PaneNameInput(_))),
+        (s("Split down"), s("Down"), action_key!(km, A::NewPane(Some(Dir::Down)), to_normal!())),
+        (s("Split right"), s("Right"), action_key!(km, A::NewPane(Some(Dir::Right)), to_normal!())),
+        (s("Fullscreen"), s("Fullscreen"), action_key!(km, A::ToggleFocusFullscreen, to_normal!())),
+        (s("Frames"), s("Frames"), action_key!(km, A::TogglePaneFrames, to_normal!())),
+        (s("Floating toggle"), s("Floating"), action_key!(km, A::ToggleFloatingPanes, to_normal!())),
+        (s("Embed pane"), s("Embed"), action_key!(km, A::TogglePaneEmbedOrFloating, to_normal!())),
+        (s("Next"), s("Next"), action_key!(km, A::SwitchFocus)),
+    ]} else if mi.mode == IM::Tab { vec![
+        (s("Move focus"), s("Move"), action_key!(km, A::GoToPreviousTab).into_iter()
+                    .chain(action_key!(km, A::GoToNextTab).into_iter()).collect()),
+        (s("New"), s("New"), action_key!(km, A::NewTab(None), to_normal!())),
+        (s("Close"), s("Close"), action_key!(km, A::CloseTab, to_normal!())),
+        (s("Rename"), s("Rename"), action_key!(km, A::SwitchToMode(IM::RenameTab), A::TabNameInput(_))),
+        (s("Sync"), s("Sync"), action_key!(km, A::ToggleActiveSyncTab, to_normal!())),
+        (s("Toggle"), s("Toggle"), action_key!(km, A::ToggleTab)),
+    ]} else if mi.mode == IM::Resize { vec![
+        (s("Resize"), s("Resize"), action_key!(km, A::Resize(RDir::Left)).into_iter()
+                    .chain(action_key!(km, A::Resize(RDir::Down)).into_iter())
+                    .chain(action_key!(km, A::Resize(RDir::Up)).into_iter())
+                    .chain(action_key!(km, A::Resize(RDir::Right)).into_iter())
+                    .collect::<Vec<Key>>()),
+        (s("Increase/Decrease size"), s("Increase/Decrease"),
+            action_key!(km, A::Resize(RDir::Increase)).into_iter()
+                    .chain(action_key!(km, A::Resize(RDir::Decrease)).into_iter()).collect()),
+    ]} else if mi.mode == IM::Move { vec![
+        (s("Move"), s("Move"), action_key!(km, Action::MovePane(Some(_)))),
+        (s("Next pane"), s("Next"), action_key!(km, Action::MovePane(None))),
+    ]} else if mi.mode == IM::Scroll { vec![
+        (s("Scroll"), s("Scroll"), action_key!(km, Action::ScrollDown).into_iter()
+                    .chain(action_key!(km, Action::ScrollUp).into_iter()).collect()),
+        (s("Scroll page"), s("Scroll"), action_key!(km, Action::PageScrollDown).into_iter()
+                    .chain(action_key!(km, Action::PageScrollUp).into_iter()).collect()),
+        (s("Scroll half page"), s("Scroll"), action_key!(km, Action::HalfPageScrollDown).into_iter()
+                    .chain(action_key!(km, Action::HalfPageScrollUp).into_iter()).collect()),
+        (s("Edit scrollback in default editor"), s("Edit"),
+            action_key!(km, Action::EditScrollback, to_normal!())),
+    ]} else if mi.mode == IM::Scroll { vec![
+        (s("Detach"), s("Detach"), action_key!(km, Action::Detach)),
+    ]} else { vec![] };
 }
 
 fn full_shortcut_list(help: &ModeInfo, tip: TipFn) -> LinePart {
