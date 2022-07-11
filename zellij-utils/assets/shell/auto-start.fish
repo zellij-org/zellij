13ec619ccb9799692cbdb1463958f520c4e6a219
Tassilo Horn <tsdh@gnu.org>
2022-07-11 21:37:20 +0200

    fix: add usage comment to fish shell auto-start snippet (#1574)

--- zellij-utils/assets/shell/auto-start.fish
+++ zellij-utils/assets/shell/auto-start.fish
@@ -1,3 +1,11 @@
+# The following snippet is meant to be used like this in your fish config:
+#
+# if status is-interactive
+#     # Configure auto-attach/exit to your likings (default is off).
+#     # set ZELLIJ_AUTO_ATTACH true
+#     # set ZELLIJ_AUTO_EXIT true
+#     eval (zellij setup --generate-auto-start fish | string collect)
+# end
 if not set -q ZELLIJ
     if test "$ZELLIJ_AUTO_ATTACH" = "true"
         zellij attach -c
