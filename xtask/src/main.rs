5509f995e6baebd92ed3024db95a3317f4f57978
Andreas Hartmann <hartan@7x.de>
2022-08-04 16:27:25 +0200

    xtask/main: Integrate new jobs
    and add documentation.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,18 +1,50 @@
 //! See <https://github.com/matklad/cargo-xtask/>.
 //!
-//! This binary defines various auxiliary build commands, which are not
-//! expressible with just `cargo`. Notably, it provides tests via `cargo test -p xtask`
-//! for code generation and `cargo xtask install` for installation of
-//! rust-analyzer server and client.
+//! This binary defines various auxiliary build commands, which are not expressible with just
+//! `cargo`. Notably, it provides tests via `cargo test -p xtask` for code generation and `cargo
+//! xtask install` for installation of rust-analyzer server and client.
 //!
-//! This binary is integrated into the `cargo` command line by using an alias in
-//! `.cargo/config`.
+//! This binary is integrated into the `cargo` command line by using an alias in `.cargo/config`.
+// Current default "flow":
+// - format-flow: `cargo fmt`
+// - format-toml-conditioned-flow: ??
+// - build: `cargo build`
+// - test: `cargo test`
+// - clippy: `cargo clippy --all-targets --all-features -- --deny warnings $@`
+//
+// # Install flow:
+// - build-plugins-release: `cargo build --release ...`
+// - wasm-opt-plugins: `wasm-opt ...`
+// - build-release: `cargo build --release`
+// - install-mandown: `cargo install mandown`
+// - manpage: |
+//      mkdir -p ${root_dir}/assets/man
+//      mandown ${root_dir}/docs/MANPAGE.md 1 > ${root_dir}/assets/man/zellij.1
+// - install: `cp target/release/zellij "$1"`
+//
+// # Release flow:
+// - workspace: cargo make --profile development -- release
+//
+// # Publish flow:
+// - update-default-config:
+// - build-plugins-release: `cargo build --release ...`
+// - wasm-opt-plugins: `wasm-opt ...`
+// - release-commit:
+//      - commit-all: `git commit -aem "chore(release): v${CRATE_VERSION}"`
+//      - tag-release: `git tag --annotate --message "Version ${CRATE_VERSION}"
+//      "v${CRATE_VERSION}"`
+//      - `git push --atomic origin main "v${CRATE_VERSION}"`
+// - publish-zellij: `cargo publish [tile, client, server, utils, tile-utils, zellij]`
 
 mod build;
+mod clippy;
 mod dist;
 mod flags;
+mod format;
+mod pipelines;
 mod test;
 
+use anyhow::Context;
 use std::{
     env,
     path::{Path, PathBuf},
@@ -44,8 +76,14 @@ fn main() -> anyhow::Result<()> {
             println!("{}", flags::Xtask::HELP);
             Ok(())
         },
-        flags::XtaskCmd::Build(flags) => build::build(&shell, flags),
-        flags::XtaskCmd::Test(flags) => test::test(&shell, flags),
+        flags::XtaskCmd::Build(flags) => build::build(shell, flags),
+        flags::XtaskCmd::Clippy(flags) => clippy::clippy(shell, flags),
+        flags::XtaskCmd::Format(flags) => format::format(shell, flags),
+        flags::XtaskCmd::Test(flags) => test::test(shell, flags),
+        // Pipelines
+        // These are composite commands, made up of multiple "stages" defined above.
+        flags::XtaskCmd::Make(flags) => pipelines::make(shell, flags),
+        flags::XtaskCmd::Install(flags) => pipelines::install(shell, flags),
         _ => unimplemented!(),
     }
 }
