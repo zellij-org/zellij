d074bb1cda819bd660b76b3be54b4db10d0eeed4
Aram Drevekenin <aram@poor.dev>
2022-10-14 15:08:32 +0200

    feat(layouts): global cwd (#1798)
    * feat(layouts): allow defining a global cwd
    
    * feat(layouts): allow passing global cwd from cli
    
    * style(fmt): rustfmt
    
    * fix(layouts): error on mixed cwd and pane children

--- zellij-utils/src/input/layout.rs
+++ zellij-utils/src/input/layout.rs
@@ -62,6 +62,41 @@ pub enum Run {
     Plugin(RunPlugin),
     #[serde(rename = "command")]
     Command(RunCommand),
+    Cwd(PathBuf),
+}
+
+impl Run {
+    pub fn merge(base: &Option<Run>, other: &Option<Run>) -> Option<Run> {
+        // TODO: handle Plugin variants once there's a need
+        match (base, other) {
+            (Some(Run::Command(base_run_command)), Some(Run::Command(other_run_command))) => {
+                let mut merged = other_run_command.clone();
+                if merged.cwd.is_none() && base_run_command.cwd.is_some() {
+                    merged.cwd = base_run_command.cwd.clone();
+                }
+                if merged.args.is_empty() && !base_run_command.args.is_empty() {
+                    merged.args = base_run_command.args.clone();
+                }
+                Some(Run::Command(merged))
+            },
+            (Some(Run::Command(base_run_command)), Some(Run::Cwd(other_cwd))) => {
+                let mut merged = base_run_command.clone();
+                merged.cwd = Some(other_cwd.clone());
+                Some(Run::Command(merged))
+            },
+            (Some(Run::Cwd(base_cwd)), Some(Run::Command(other_command))) => {
+                let mut merged = other_command.clone();
+                if merged.cwd.is_none() {
+                    merged.cwd = Some(base_cwd.clone());
+                }
+                Some(Run::Command(merged))
+            },
+            (Some(_base), Some(other)) => Some(other.clone()),
+            (Some(base), _) => Some(base.clone()),
+            (None, Some(other)) => Some(other.clone()),
+            (None, None) => None,
+        }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
@@ -252,12 +287,16 @@ impl Layout {
     ) -> Result<(Layout, Config), ConfigError> {
         let (path_to_raw_layout, raw_layout) =
             Layout::stringified_from_path_or_default(layout_path, layout_dir)?;
-        let layout = Layout::from_kdl(&raw_layout, path_to_raw_layout)?;
+        let layout = Layout::from_kdl(&raw_layout, path_to_raw_layout, None)?;
         let config = Config::from_kdl(&raw_layout, Some(config))?; // this merges the two config, with
         Ok((layout, config))
     }
-    pub fn from_str(raw: &str, path_to_raw_layout: String) -> Result<Layout, ConfigError> {
-        Layout::from_kdl(raw, path_to_raw_layout)
+    pub fn from_str(
+        raw: &str,
+        path_to_raw_layout: String,
+        cwd: Option<PathBuf>,
+    ) -> Result<Layout, ConfigError> {
+        Layout::from_kdl(raw, path_to_raw_layout, cwd)
     }
     pub fn stringified_from_dir(
         layout: &PathBuf,
