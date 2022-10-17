1c3d1ac72bc258da2f91c1fbcc13f3a2c0d91d54
Aram Drevekenin <aram@poor.dev>
2022-10-17 17:04:32 +0200

    feat(cli): zellij run completions for fish

--- zellij-utils/assets/completions/comp.fish
+++ zellij-utils/assets/completions/comp.fish
@@ -6,3 +6,19 @@ complete -c zellij -n "__fish_seen_subcommand_from a" -f -a "(__fish_complete_se
 complete -c zellij -n "__fish_seen_subcommand_from kill-session" -f -a "(__fish_complete_sessions)" -d "Session"
 complete -c zellij -n "__fish_seen_subcommand_from k" -f -a "(__fish_complete_sessions)" -d "Session"
 complete -c zellij -n "__fish_seen_subcommand_from setup" -l "generate-completion" -x -a "bash elvish fish zsh powershell" -d "Shell"
+function zp
+  # zellij pane
+  command zellij run --name "$argv" -- fish -c "$argv"
+end
+function zpf
+  # zellij pane floating
+  command zellij run --name "$argv" --floating -- fish -c "$argv"
+end
+function zo
+  # zellij open
+  command zellij edit $argv
+end
+function zof
+  # zellij open floating
+  command zellij edit --floating $argv
+end
