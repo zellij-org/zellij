7880416d52bd2bb42731b449c06aa569d64369ba
Andreas Hartmann <hartan@7x.de>
2022-10-23 10:10:21 +0200

    server/wasm_vm: Prettier error handling
    in call to `wasmer::Function::call` in case a plugin version mismatch
    can occur.

--- zellij-server/src/wasm_vm.rs
+++ zellij-server/src/wasm_vm.rs
@@ -222,7 +222,16 @@ pub(crate) fn wasm_thread_main(
                             .get_function("update")
                             .with_context(err_context)?;
                         wasi_write_object(&plugin_env.wasi_env, &event);
-                        update.call(&[]).with_context(err_context)?;
+                        update.call(&[]).or_else::<anyError, _>(|e| {
+                            match e.downcast::<serde_json::Error>() {
+                                Ok(_) => panic!("{}", 
+                                    anyError::new(VersionMismatchError::new(
+                                        VERSION,
+                                        "Unavailable",
+                                        &plugin_env.plugin.path
+                                    ))),
+                                Err(e) => Err(e).with_context(err_context)
+                            }})?;
                     }
                 }
                 drop(bus.senders.send_to_screen(ScreenInstruction::Render));
