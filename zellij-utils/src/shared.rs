e1842b1d73f2c43414edae02f0815f821d31e4e0
Andreas Hartmann <hartan@7x.de>
2022-11-10 14:43:38 +0100

    utils/shared: fix missing "set_permissions"
    function when not on unix systems.

--- zellij-utils/src/shared.rs
+++ zellij-utils/src/shared.rs
@@ -24,6 +24,11 @@ mod unix_only {
     }
 }
 
+#[cfg(not(unix))]
+pub fn set_permissions(path: &std::path::Path, mode: u32) -> std::io::Result<()> {
+    Ok(())
+}
+
 pub fn ansi_len(s: &str) -> usize {
     from_utf8(&strip(s).unwrap()).unwrap().width()
 }
