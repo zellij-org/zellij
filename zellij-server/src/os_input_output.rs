8f812a65b2764026789e23f2113623977e778f2a
Aram Drevekenin <aram@poor.dev>
2023-02-05 13:54:52 +0100

    style(comment): redraw-on-backpressure comment

--- zellij-server/src/os_input_output.rs
+++ zellij-server/src/os_input_output.rs
@@ -350,7 +350,8 @@ impl ClientSender {
         // 5000 instead. This decision was made because it was found that a queue of depth 5000
         // doesn't cause noticable increase in RAM usage, but there's no reason beyond that. If in
         // the future this is found to fill up too quickly again, it may be worthwhile to increase
-        // the size even further. We, the zellij maintainers, have decided against an unbounded
+        // the size even further (or better yet, implement a redraw-on-backpressure mechanism).
+        // We, the zellij maintainers, have decided against an unbounded
         // queue for the time being because we want to prevent e.g. the whole session being killed
         // (by OOM-killers or some other mechanism) just because a single client doesn't respond.
         let (client_buffer_sender, client_buffer_receiver) = channels::bounded(5000);
