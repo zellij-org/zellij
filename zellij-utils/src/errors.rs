b6aefe065eb81d40348bfc164a38ef76db56f105
Aram Drevekenin <aram@poor.dev>
2023-04-18 11:52:44 +0200

    style(fmt): cleanups

--- zellij-utils/src/errors.rs
+++ zellij-utils/src/errors.rs
@@ -494,8 +494,6 @@ open an issue on GitHub:
     #[error("Client {client_id} is too slow to handle incoming messages")]
     ClientTooSlow { client_id: u16 },
 
-    //     #[error("The plugin is already loading")]
-    //     PluginCurrentlyLoading,
     #[error("The plugin does not exist")]
     PluginDoesNotExist,
 }
