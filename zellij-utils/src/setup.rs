34bb2fa39d2032f125eaffa0cc816f5104ab7f7b
Imbris <imbrisf@gmail.com>
2023-04-28 15:56:04 -0400

    If `setup --clean` is used, avoid loading from the user's theme directory.

--- zellij-utils/src/setup.rs
+++ zellij-utils/src/setup.rs
@@ -322,14 +322,16 @@ impl Setup {
 
         config.themes = get_default_themes().merge(config.themes);
 
-        let user_theme_dir = config_options.theme_dir.clone().or_else(|| {
-            get_theme_dir(cli_args.config_dir.clone().or_else(find_default_config_dir))
-                // If theme dir is not explicitly specified in config_options,
-                // only try to use it if it exists.
-                .filter(|dir| dir.exists())
-        });
-        if let Some(user_theme_dir) = user_theme_dir {
-            config.themes = config.themes.merge(Themes::from_dir(user_theme_dir)?);
+        if let Some(Command::Setup(Setup { clean: false, .. })) = &cli_args.command {
+            let user_theme_dir = config_options.theme_dir.clone().or_else(|| {
+                get_theme_dir(cli_args.config_dir.clone().or_else(find_default_config_dir))
+                    // If theme dir is not explicitly specified in config_options,
+                    // only try to use it if it exists.
+                    .filter(|dir| dir.exists())
+            });
+            if let Some(user_theme_dir) = user_theme_dir {
+                config.themes = config.themes.merge(Themes::from_dir(user_theme_dir)?);
+            }
         }
 
         if let Some(Command::Setup(ref setup)) = &cli_args.command {
