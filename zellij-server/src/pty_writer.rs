02824d4548f5e6ef3109a3a61be33b06e8ff884c
Aram Drevekenin <aram@poor.dev>
2023-07-12 20:03:49 +0200

    fix(rendering): occasional glitches while resizing

--- zellij-server/src/pty_writer.rs
+++ zellij-server/src/pty_writer.rs
@@ -2,9 +2,16 @@ use zellij_utils::errors::{prelude::*, ContextType, PtyWriteContext};
 
 use crate::thread_bus::Bus;
 
+// we separate these instruction to a different thread because some programs get deadlocked if
+// you write into their STDIN while reading from their STDOUT (I'm looking at you, vim)
+// while the same has not been observed to happen with resizes, it could conceivably happen and we have this
+// here anyway, so
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum PtyWriteInstruction {
     Write(Vec<u8>, u32),
+    ResizePty(u32, u16, u16, Option<u16>, Option<u16>), // terminal_id, columns, rows, pixel width, pixel height
+    StartCachingResizes,
+    ApplyCachedResizes,
     Exit,
 }
 
@@ -12,6 +19,9 @@ impl From<&PtyWriteInstruction> for PtyWriteContext {
     fn from(tty_write_instruction: &PtyWriteInstruction) -> Self {
         match *tty_write_instruction {
             PtyWriteInstruction::Write(..) => PtyWriteContext::Write,
+            PtyWriteInstruction::ResizePty(..) => PtyWriteContext::ResizePty,
+            PtyWriteInstruction::ApplyCachedResizes => PtyWriteContext::ApplyCachedResizes,
+            PtyWriteInstruction::StartCachingResizes => PtyWriteContext::StartCachingResizes,
             PtyWriteInstruction::Exit => PtyWriteContext::Exit,
         }
     }
@@ -23,7 +33,7 @@ pub(crate) fn pty_writer_main(bus: Bus<PtyWriteInstruction>) -> Result<()> {
     loop {
         let (event, mut err_ctx) = bus.recv().with_context(err_context)?;
         err_ctx.add_call(ContextType::PtyWrite((&event).into()));
-        let os_input = bus
+        let mut os_input = bus
             .os_input
             .clone()
             .context("no OS input API found")
@@ -38,6 +48,38 @@ pub(crate) fn pty_writer_main(bus: Bus<PtyWriteInstruction>) -> Result<()> {
                     .tcdrain(terminal_id)
                     .with_context(err_context)
                     .non_fatal();
+            },
+            PtyWriteInstruction::ResizePty(
+                terminal_id,
+                columns,
+                rows,
+                width_in_pixels,
+                height_in_pixels,
+            ) => {
+                os_input
+                    .set_terminal_size_using_terminal_id(
+                        terminal_id,
+                        columns,
+                        rows,
+                        width_in_pixels,
+                        height_in_pixels,
+                    )
+                    .with_context(err_context)
+                    .non_fatal();
+            },
+            PtyWriteInstruction::StartCachingResizes => {
+                // we do this because there are some logic traps inside the screen/tab/layout code
+                // the cause multiple resizes to be sent to the pty - while the last one is always
+                // the correct one, many programs and shells debounce those (I guess due to the
+                // trauma of dealing with GUI resizes of the controlling terminal window), and this
+                // then causes glitches and missing redraws
+                // so we do this to play nice and always only send the last resize instruction to
+                // each pane
+                // the logic for this happens in the main Screen event loop
+                os_input.cache_resizes();
+            },
+            PtyWriteInstruction::ApplyCachedResizes => {
+                os_input.apply_cached_resizes();
             },
             PtyWriteInstruction::Exit => {
                 return Ok(());
