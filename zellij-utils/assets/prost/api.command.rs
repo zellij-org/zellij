26a3974681c4f7d4ac293865bff2c3d94cc3392f
har7an <99636919+har7an@users.noreply.github.com>
2023-08-28 06:24:27 +0000

    xtask/pipeline: Fix publish task (#2711)
    * xtask/pipeline: Fix publish task
    
    which was previously stuck in an infinite loop after successfully
    publishing a crate. The error originated in the code only checking for
    error conditions but not breaking out of the inner infinite loop in case
    of success.
    
    * xtask: Improve publish failure UX
    
    by offering the user more actions to choose from when an error occured.
    
    * utils/assets: Add generated prost files to assets
    
    to make sure they're available at build time and are picked up by all
    components. It seems we hit some strange bug with the build script
    where, when running `cargo publish --dry-run` the build script **is
    not** run before regularly compiling zellij-utils. This shouldn't happen
    according to the docs, but I cannot explain what's causing it. So we're
    using this as a workaround for now to make a smooth release.
    
    * xtask: Prevent accidental git commit deletion
    
    when dry-running a publish.
    
    * utils: Add comments to protobuf-related code
    
    to explain why these changes were performed. The comments all include a
    link to an issue comment explaining the situation in greater detail.
    
    * xtask: Build protobuf definitions
    
    when building any part of the project, similar to how we build the
    plugins when required. This should ensure that all crates built through
    `cargo xtask` (which is the officially supported build method) will
    receive up-to-date protobuf definitions.

---
+++ zellij-utils/assets/prost/api.command.rs
@@ -0,0 +1,10 @@
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Command {
+    #[prost(string, tag = "1")]
+    pub path: ::prost::alloc::string::String,
+    #[prost(string, repeated, tag = "2")]
+    pub args: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+    #[prost(string, optional, tag = "3")]
+    pub cwd: ::core::option::Option<::prost::alloc::string::String>,
+}
---
+++ zellij-utils/assets/prost/api.plugin_permission.rs
@@ -0,0 +1,38 @@
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
+#[repr(i32)]
+pub enum PermissionType {
+    ReadApplicationState = 0,
+    ChangeApplicationState = 1,
+    OpenFiles = 2,
+    RunCommands = 3,
+    OpenTerminalsOrPlugins = 4,
+    WriteToStdin = 5,
+}
+impl PermissionType {
+    /// String value of the enum field names used in the ProtoBuf definition.
+    ///
+    /// The values are not transformed in any way and thus are considered stable
+    /// (if the ProtoBuf definition does not change) and safe for programmatic use.
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            PermissionType::ReadApplicationState => "ReadApplicationState",
+            PermissionType::ChangeApplicationState => "ChangeApplicationState",
+            PermissionType::OpenFiles => "OpenFiles",
+            PermissionType::RunCommands => "RunCommands",
+            PermissionType::OpenTerminalsOrPlugins => "OpenTerminalsOrPlugins",
+            PermissionType::WriteToStdin => "WriteToStdin",
+        }
+    }
+    /// Creates an enum from field names used in the ProtoBuf definition.
+    pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
+        match value {
+            "ReadApplicationState" => Some(Self::ReadApplicationState),
+            "ChangeApplicationState" => Some(Self::ChangeApplicationState),
+            "OpenFiles" => Some(Self::OpenFiles),
+            "RunCommands" => Some(Self::RunCommands),
+            "OpenTerminalsOrPlugins" => Some(Self::OpenTerminalsOrPlugins),
+            "WriteToStdin" => Some(Self::WriteToStdin),
+            _ => None,
+        }
+    }
+}
