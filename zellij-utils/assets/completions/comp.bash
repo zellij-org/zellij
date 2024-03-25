8b2263ecf3906ebb7f14ba5f68641966eb06297c
Cristian Dominguez <6853656+cristiand391@users.noreply.github.com>
2024-03-25 08:31:02 -0300

    feat(cli): add run/edit in-place functions (#3038)
    Co-authored-by: Aram Drevekenin <aram@poor.dev>

--- zellij-utils/assets/completions/comp.bash
+++ zellij-utils/assets/completions/comp.bash
@@ -1,7 +1,9 @@
 function zr () { zellij run --name "$*" -- bash -ic "$*";}
 function zrf () { zellij run --name "$*" --floating -- bash -ic "$*";}
+function zri () { zellij run --name "$*" --in-place -- bash -ic "$*";}
 function ze () { zellij edit "$*";}
 function zef () { zellij edit --floating "$*";}
+function zei () { zellij edit --in-place "$*";}
 function zpipe () { 
   if [ -z "$1" ]; then
     zellij pipe;
--- zellij-utils/assets/completions/comp.zsh
+++ zellij-utils/assets/completions/comp.zsh
@@ -1,7 +1,9 @@
 function zr () { zellij run --name "$*" -- zsh -ic "$*";}
 function zrf () { zellij run --name "$*" --floating -- zsh -ic "$*";}
+function zri () { zellij run --name "$*" --in-place -- zsh -ic "$*";}
 function ze () { zellij edit "$*";}
 function zef () { zellij edit --floating "$*";}
+function zei () { zellij edit --in-place "$*";}
 function zpipe () { 
   if [ -z "$1" ]; then
     zellij pipe;
