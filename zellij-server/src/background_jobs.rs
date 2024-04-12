b568bfb16ec77f5a717718b7c3778ea396199690
Aram Drevekenin <aram@poor.dev>
2024-04-12 15:45:03 +0200

    feat(config): allow disabling writing session metadata to disk

--- zellij-server/src/background_jobs.rs
+++ zellij-server/src/background_jobs.rs
@@ -84,6 +84,7 @@ static DEFAULT_SERIALIZATION_INTERVAL: u64 = 60000;
 pub(crate) fn background_jobs_main(
     bus: Bus<BackgroundJob>,
     serialization_interval: Option<u64>,
+    disable_session_metadata: bool,
 ) -> Result<()> {
     let err_context = || "failed to write to pty".to_string();
     let mut running_jobs: HashMap<BackgroundJob, Instant> = HashMap::new();
@@ -175,11 +176,13 @@ pub(crate) fn background_jobs_main(
                             let current_session_info = current_session_info.lock().unwrap().clone();
                             let current_session_layout =
                                 current_session_layout.lock().unwrap().clone();
-                            write_session_state_to_disk(
-                                current_session_name.clone(),
-                                current_session_info,
-                                current_session_layout,
-                            );
+                            if !disable_session_metadata {
+                                write_session_state_to_disk(
+                                    current_session_name.clone(),
+                                    current_session_info,
+                                    current_session_layout,
+                                );
+                            }
                             let session_infos_on_machine =
                                 read_other_live_session_states(&current_session_name);
                             let resurrectable_sessions =
