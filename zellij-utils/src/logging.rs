efd3fe2c263f3b57ebcc9e4ae5d2b44c7c4961e4
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-05-15 14:27:43 +0200

    Reduce verbosity of wasmtime_wasi logs

--- zellij-utils/src/logging.rs
+++ zellij-utils/src/logging.rs
@@ -68,10 +68,16 @@ pub fn configure_logger() {
         .unwrap();
 
     // Set the default logging level to "info" and log it to zellij.log file
+    // Decrease verbosity for `wasmtime_wasi` module because it has a lot of useless info logs
     // For `zellij_server::logging_pipe`, we use custom format as we use logging macros to forward stderr output from plugins
     let config = Config::builder()
         .appender(Appender::builder().build("logFile", Box::new(log_file)))
         .appender(Appender::builder().build("logPlugin", Box::new(log_plugin)))
+        .logger(
+            Logger::builder()
+                .appender("logPlugin")
+                .build("wasmtime_wasi", LevelFilter::Warn),
+        )
         .logger(
             Logger::builder()
                 .appender("logPlugin")
