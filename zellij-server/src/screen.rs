304365337908a97d8e5c58782d86b2827848fcd6
Mark Grey <mgthesecond@spotify.com>
2024-07-31 19:34:47 -0400

    fix tab bar more text

--- zellij-server/src/screen.rs
+++ zellij-server/src/screen.rs
@@ -2440,7 +2440,7 @@ impl Screen {
         &mut self,
         new_keybinds: Keybinds,
         new_default_mode: InputMode,
-        theme: Palette,
+        theme: Styling,
         simplified_ui: bool,
         default_shell: Option<PathBuf>,
         pane_frames: bool,
