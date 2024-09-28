f374ff2f518d7c677677e9069e07480cadf853ba
Mark Grey <mgthesecond@spotify.com>
2024-09-28 15:22:32 -0400

    repair field flattening for theme

--- zellij-utils/src/kdl/mod.rs
+++ zellij-utils/src/kdl/mod.rs
@@ -4178,7 +4178,9 @@ impl Themes {
             }
             has_themes = true;
             let mut current_theme_node = KdlNode::new(theme_name.clone());
+            let mut current_theme_node_styling = KdlNode::new("styling");
             let mut current_theme_node_children = KdlDocument::new();
+            let mut current_theme_node_styling_children = KdlDocument::new();
             current_theme_node_children
                 .nodes_mut()
                 .push(theme.palette.text_unselected.to_kdl("text_unselected"));
@@ -4227,7 +4229,11 @@ impl Themes {
             current_theme_node_children
                 .nodes_mut()
                 .push(theme.palette.multiplayer_user_colors.to_kdl());
-            current_theme_node.set_children(current_theme_node_children);
+            current_theme_node_styling.set_children(current_theme_node_children);
+            current_theme_node_styling_children
+                .nodes_mut()
+                .push(current_theme_node_styling);
+            current_theme_node.set_children(current_theme_node_styling_children);
             themes.nodes_mut().push(current_theme_node);
         }
         if has_themes {
