836a6c0e66ed30f8de602af5b03da9722340f858
Andreas Hartmann <hartan@7x.de>
2025-03-18 05:22:58 +0100

    deps(utils): Remove `once_cell` dependency.

--- zellij-utils/src/consts.rs
+++ zellij-utils/src/consts.rs
@@ -4,8 +4,7 @@ use crate::home::find_default_config_dir;
 use directories::ProjectDirs;
 use include_dir::{include_dir, Dir};
 use lazy_static::lazy_static;
-use once_cell::sync::OnceCell;
-use std::path::PathBuf;
+use std::{sync::OnceLock, path::PathBuf};
 use uuid::Uuid;
 
 pub const ZELLIJ_CONFIG_FILE_ENV: &str = "ZELLIJ_CONFIG_FILE";
@@ -13,8 +12,8 @@ pub const ZELLIJ_CONFIG_DIR_ENV: &str = "ZELLIJ_CONFIG_DIR";
 pub const ZELLIJ_LAYOUT_DIR_ENV: &str = "ZELLIJ_LAYOUT_DIR";
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const DEFAULT_SCROLL_BUFFER_SIZE: usize = 10_000;
-pub static SCROLL_BUFFER_SIZE: OnceCell<usize> = OnceCell::new();
-pub static DEBUG_MODE: OnceCell<bool> = OnceCell::new();
+pub static SCROLL_BUFFER_SIZE: OnceLock<usize> = OnceLock::new();
+pub static DEBUG_MODE: OnceLock<bool> = OnceLock::new();
 
 pub const SYSTEM_DEFAULT_CONFIG_DIR: &str = "/etc/zellij";
 pub const SYSTEM_DEFAULT_DATA_DIR_PREFIX: &str = system_default_data_dir();
