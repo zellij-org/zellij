4a4b4e38c20dfc36c3325c222cbd32b751cf6fc6
Aram Drevekenin <aram@poor.dev>
2025-03-26 10:26:36 +0100

    refactor ui

--- default-plugins/share/src/main.rs
+++ default-plugins/share/src/main.rs
@@ -5,6 +5,7 @@ use std::collections::BTreeMap;
 #[derive(Debug, Default)]
 struct App {
     web_server_started: bool,
+    session_is_shared: bool,
     session_name: Option<String>,
 }
 
@@ -26,7 +27,11 @@ impl ZellijPlugin for App {
             EventType::Key,
             EventType::ModeUpdate,
             EventType::WebServerStarted,
+            EventType::Timer,
         ]);
+        query_web_server();
+        list_web_sessions();
+        set_timeout(0.5);
     }
     fn update(&mut self, event: Event) -> bool {
         let mut should_render = false;
@@ -34,7 +39,7 @@ impl ZellijPlugin for App {
             Event::ModeUpdate(mode_info) => {
                 self.session_name = mode_info.session_name;
                 if let Some(session_is_shared) = mode_info.session_is_shared {
-                    self.web_server_started = session_is_shared;
+                    self.session_is_shared = session_is_shared;
                 }
             },
             Event::WebServerStarted => {
@@ -49,6 +54,11 @@ impl ZellijPlugin for App {
                     _ => {}
                 }
             },
+            Event::Timer(_) => {
+                query_web_server();
+                list_web_sessions();
+                set_timeout(0.5);
+            }
             _ => {},
         }
         should_render
@@ -72,62 +82,97 @@ impl ZellijPlugin for App {
         //  - Open in browser
         //  - Stop sharing
         //
-        let web_server_status_title = "Web server status:";
-        let web_server_status_value = if self.web_server_started { "RUNNING" } else { "NOT RUNNING" };
-        let url_title = "URL:";
-        let url_value = format!("http://localhost:8082/");
-        let current_session_title = "Current session:";
-        let current_session_value = if self.web_server_started { "SHARING" } else { "NOT SHARING" }; // TODO: this is not accurate
-        let session_url_title = "Session URL:";
-        let session_url_value = match &self.session_name {
-            Some(session_name) => {
-                format!("http://localhost:8082/{}", session_name)
-            },
-            None => {
-                format!("http://localhost:8082/")
-            }
+        let (web_server_status_items, web_server_items_max_len) = self.render_web_server_status();
+        let (current_session_status_items, current_session_items_max_len) = self.render_current_session_status();
+        let (all_sessions_list_title, all_sessions_list_items, all_sessions_items_max_len) = self.render_all_sessions_list();
+        let max_item_width = std::cmp::max(web_server_items_max_len, std::cmp::max(current_session_items_max_len, all_sessions_items_max_len));
+        let item_count = web_server_status_items.len() + current_session_status_items.len() + 1 + all_sessions_list_items.len();
+        let base_x = cols.saturating_sub(max_item_width) / 2;
+        let base_y = rows.saturating_sub(item_count + 2) / 2; // the + 2 are the line spaces
+                                                              // between items
+        let mut current_y = base_y;
+        for item in web_server_status_items {
+            print_text_with_coordinates(item, base_x, current_y, None, None);
+            current_y += 1;
+        }
+        current_y += 1; // space between items
+        for item in current_session_status_items {
+            print_text_with_coordinates(item, base_x, current_y, None, None);
+            current_y += 1;
+        }
+        current_y += 1; // space between items
+        print_text_with_coordinates(all_sessions_list_title, base_x, current_y, None, None);
+        current_y += 1;
+        print_nested_list_with_coordinates(all_sessions_list_items, base_x, current_y, None, None);
+    }
+}
+
+// render methods, return UI components and the width of the widest one
+impl App {
+    pub fn render_web_server_status(&self) -> (Vec<Text>, usize) {
+        let mut max_len = 0;
+        let web_server_status_line = if self.web_server_started {
+            let title = "Web server status: ";
+            let value = "RUNNING";
+            max_len = std::cmp::max(max_len, title.chars().count() + value.chars().count());
+            Text::new(format!("{}{}", title, value)).color_range(0, ..title.chars().count()).color_range(3, title.chars().count()..)
+        } else {
+            let title = "Web server status: ";
+            let value = "NOT RUNNING";
+            max_len = std::cmp::max(max_len, title.chars().count() + value.chars().count());
+            Text::new(format!("{}{}", title, value)).color_range(0, ..title.chars().count()).color_range(3, title.chars().count()..)
         };
+        let info_line = if self.web_server_started {
+            let title = "URL:";
+            let value = format!("http://localhost:8082/");
+            max_len = std::cmp::max(max_len, title.chars().count() + value.chars().count());
+            Text::new(format!("{}{}", title, value)).color_range(0, ..title.chars().count())
+        } else {
+            let text = "Press <ENTER> to start";
+            max_len = std::cmp::max(max_len, text.chars().count());
+            Text::new(text).color_range(3, 6..=12)
+        };
+        (vec![web_server_status_line, info_line], max_len)
+    }
+    pub fn render_current_session_status(&self) -> (Vec<Text>, usize) {
+        let mut max_len = 0;
+        let status_line = if self.session_is_shared {
+            let title = "Current session: ";
+            let value = "SHARING";
+            max_len = std::cmp::max(max_len, title.chars().count() + value.chars().count());
+            Text::new(format!("{}{}", title, value)).color_range(0, ..title.chars().count()).color_range(3, title.chars().count()..)
+        } else {
+            let title = "Current session: ";
+            let value = "NOT SHARING";
+            max_len = std::cmp::max(max_len, title.chars().count() + value.chars().count());
+            Text::new(format!("{}{}", title, value)).color_range(0, ..title.chars().count()).color_range(3, title.chars().count()..)
+        };
+        let info_line = if self.session_is_shared {
+            let title = "Session URL: ";
+            let value = format!("http://localhost:8082/{}", self.session_name.clone().unwrap_or_else(|| "".to_owned()));
+            max_len = std::cmp::max(max_len, title.chars().count() + value.chars().count());
+            Text::new(format!("{}{}", title, value)).color_range(0, ..title.chars().count())
+        } else {
+            let text = "Press <SPACE> to share";
+            max_len = std::cmp::max(max_len, text.chars().count());
+            Text::new(text).color_range(3, 6..=12)
+        };
+        (vec![status_line, info_line], max_len)
+    }
+    pub fn render_all_sessions_list(&self) -> (Text, Vec<NestedListItem>, usize) {
         let all_sessions_title = "All web sessions:";
         let mock_sessions = vec![
             WebSessionInfo::default().with_name("session 1"),
             WebSessionInfo::default().with_name("session 2"),
             WebSessionInfo::default().with_name("session 3"),
         ];
-
-        let row_count = 7 + mock_sessions.len();
-        let base_y = rows.saturating_sub(row_count) / 2; // TODO: more accurate
-        let base_x = cols.saturating_sub(session_url_title.chars().count() + session_url_value.chars().count()) / 2; // TODO: more
-                                                                                 // accurate
-
-        print_text_with_coordinates(Text::new(&web_server_status_title).color_range(0, ..), base_x, base_y, None, None);
-        print_text_with_coordinates(Text::new(&web_server_status_value).color_range(3, ..), base_x + web_server_status_title.chars().count() + 1, base_y, None, None);
-        print_text_with_coordinates(Text::new(&url_title).color_range(0, ..), base_x, base_y + 1, None, None);
-        print_text_with_coordinates(Text::new(&url_value), base_x + url_title.chars().count() + 1, base_y + 1, None, None);
-
-        print_text_with_coordinates(Text::new(&current_session_title).color_range(0, ..), base_x, base_y + 3, None, None);
-        print_text_with_coordinates(Text::new(&current_session_value).color_range(3, ..), base_x + current_session_title.chars().count() + 1, base_y + 3, None, None);
-        print_text_with_coordinates(Text::new(&session_url_title).color_range(0, ..), base_x, base_y + 4, None, None);
-        print_text_with_coordinates(Text::new(&session_url_value), base_x + session_url_title.chars().count() + 1, base_y + 4, None, None);
-
-        print_text_with_coordinates(Text::new(&all_sessions_title).color_range(1, ..), base_x, base_y + 6, None, None);
+        let mut max_len = all_sessions_title.chars().count();
+        let all_sessions = Text::new(all_sessions_title).color_range(1, ..);
         let mut nested_list = vec![];
         for mock_session in mock_sessions {
+            max_len = std::cmp::max(mock_session.name.chars().count() + 3, max_len); // 3 is the bulletin
             nested_list.push(NestedListItem::new(mock_session.name));
         }
-        print_nested_list_with_coordinates(nested_list, base_x, base_y + 7, None, None);
+        (all_sessions, nested_list, max_len)
     }
 }
-
-// #[derive(Default)]
-// struct WebSessionInfo {
-//     name: String,
-//     web_client_count: usize,
-//     terminal_client_count: usize
-// }
-// 
-// impl WebSessionInfo {
-//     pub fn with_name(mut self, name: &str) -> Self {
-//         self.name = name.to_string();
-//         self
-//     }
-// }
