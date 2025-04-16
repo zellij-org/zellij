2127ef78e736127efc4460a84003f4879a4ca9a9
Aram Drevekenin <aram@poor.dev>
2025-04-16 16:50:44 +0200

    get plugins to also display their frame color

--- zellij-server/src/panes/plugin_pane.rs
+++ zellij-server/src/panes/plugin_pane.rs
@@ -646,6 +646,10 @@ impl Pane for PluginPane {
     fn add_red_pane_frame_color_override(&mut self, error_text: Option<String>) {
         self.pane_frame_color_override = Some((self.style.colors.exit_code_error.base, error_text));
     }
+    fn add_highlight_pane_frame_color_override(&mut self, text: Option<String>, _client_id: Option<ClientId>) {
+        // TODO: if we have a client_id, we should only highlight the frame for this client
+        self.pane_frame_color_override = Some((self.style.colors.frame_highlight.base, text));
+    }
     fn clear_pane_frame_color_override(&mut self, _client_id: Option<ClientId>) {
         // TODO: if we have a client_id, we should only clear the highlight for this client
         self.pane_frame_color_override = None;
