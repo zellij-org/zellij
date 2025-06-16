b3e5798040842ccfcafa9c851548b6b02d74a5a5
Aram Drevekenin <aram@poor.dev>
2025-06-16 10:21:58 +0200

    improve xterm.js link handling

--- zellij-client/assets/index.js
+++ zellij-client/assets/index.js
@@ -334,7 +334,11 @@ function initTerminal() {
     window.term = term;
     const fitAddon = new FitAddon.FitAddon();
     const clipboardAddon = new ClipboardAddon.ClipboardAddon();
-    const webLinksAddon = new WebLinksAddon.WebLinksAddon();
+
+    const { linkHandler, activateLink } = build_link_handler();
+    const webLinksAddon = new WebLinksAddon.WebLinksAddon(activateLink, linkHandler);
+    term.options.linkHandler = linkHandler;
+
     const webglAddon = new WebglAddon.WebglAddon();
     term.loadAddon(fitAddon);
     term.loadAddon(clipboardAddon);
@@ -389,3 +393,46 @@ async function get_client_id(token, rememberMe, has_authentication_cookie) {
       return body.web_client_id;
     }
 }
+
+function build_link_handler() {
+    let _linkPopup;
+    function removeLinkPopup (event, text, range) {
+      if (_linkPopup) {
+         _linkPopup.remove();
+         _linkPopup = undefined;
+      }
+    }
+
+    function showLinkPopup(event, text, range) {
+      let popup = document.createElement('div');
+      popup.classList.add('xterm-link-popup');
+      popup.style.position = 'absolute';
+      popup.style.top = (event.clientY + 25) + 'px';
+      popup.style.left = (event.clientX + 25) + 'px';
+      popup.style.fontSize = 'small';
+      popup.style.lineBreak = 'normal';
+      popup.style.padding = '4px';
+      popup.style.minWidth = '15em';
+      popup.style.maxWidth = '80%';
+      popup.style.border = 'thin solid';
+      popup.style.borderRadius = '6px';
+      popup.style.background = '#6c4c4c';
+      popup.style.borderColor = '#150262';
+      popup.innerText = "Shift-Click: " + text;
+      const topElement = event.target.parentNode;
+      topElement.appendChild(popup);
+      const popupHeight = popup.offsetHeight;
+      _linkPopup = popup;
+    }
+    function activateLink(event, uri) {
+      const newWindow = window.open(uri, '_blank');
+      if (newWindow) newWindow.opener = null; // prevent the opened link from gaining access to the terminal instance
+    }
+
+    let linkHandler = {};
+    linkHandler.hover = showLinkPopup;
+    linkHandler.leave = removeLinkPopup;
+    linkHandler.activate = activateLink;
+    return { linkHandler, activateLink };
+}
+
