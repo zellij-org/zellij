19c159175c12a06e3517882487caa2f834f29033
Aram Drevekenin <aram@poor.dev>
2025-07-17 11:41:17 +0200

    fix(clipboard): reap process of external command (#4298)
    * fix(clipboard): reap process of external command
    
    * docs(changelog): add PR

--- zellij-server/src/tab/copy_command.rs
+++ zellij-server/src/tab/copy_command.rs
@@ -18,16 +18,43 @@ impl CopyCommand {
         }
     }
     pub fn set(&self, value: String) -> Result<()> {
-        let process = Command::new(self.command.clone())
+        let mut process = Command::new(self.command.clone())
             .args(self.args.clone())
             .stdin(Stdio::piped())
             .spawn()
             .with_context(|| format!("couldn't spawn {}", self.command))?;
         process
             .stdin
+            .take()
             .context("could not get stdin")?
             .write_all(value.as_bytes())
             .with_context(|| format!("couldn't write to {} stdin", self.command))?;
+
+        // reap process with a 1 second timeout
+        std::thread::spawn(move || {
+            let timeout = std::time::Duration::from_secs(1);
+            let start = std::time::Instant::now();
+
+            loop {
+                match process.try_wait() {
+                    Ok(Some(_)) => {
+                        return; // Process finished normally
+                    },
+                    Ok(None) => {
+                        if start.elapsed() > timeout {
+                            let _ = process.kill();
+                            log::error!("Copy operation times out after 1 second");
+                            return;
+                        }
+                        std::thread::sleep(std::time::Duration::from_millis(50));
+                    },
+                    Err(e) => {
+                        log::error!("Clipboard failure: {}", e);
+                        return;
+                    },
+                }
+            }
+        });
 
         Ok(())
     }
