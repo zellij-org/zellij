1523f9ebf6b8606689aec1337cd3a32d49aab912
Ilya Voronin <ivoronin@gmail.com>
2026-01-22 00:13:54 +0200

    feat(ui): add optional tab indices to compact-bar

--- default-plugins/compact-bar/src/main.rs
+++ default-plugins/compact-bar/src/main.rs
@@ -60,6 +60,9 @@ struct State {
     is_first_run: bool,
     own_tab_index: Option<usize>,
     own_client_id: u16,
+
+    // Tab display options
+    show_tab_indices: bool,
 }
 
 struct TabRenderData {
@@ -130,6 +133,7 @@ impl State {
     fn initialize_configuration(&mut self, configuration: BTreeMap<String, String>) {
         self.config = configuration.clone();
         self.is_tooltip = self.parse_bool_config(CONFIG_IS_TOOLTIP, false);
+        self.show_tab_indices = self.parse_bool_config("show_tab_indices", false);
 
         if !self.is_tooltip {
             if let Some(tooltip_toggle_key) = configuration.get(CONFIG_TOGGLE_TOOLTIP_KEY) {
@@ -525,12 +529,19 @@ impl State {
                 }
             }
 
+            let tab_index = if self.show_tab_indices {
+                Some(tab.position + 1)
+            } else {
+                None
+            };
+
             let styled_tab = tab_style(
                 tab_name,
                 tab,
                 is_alternate_tab,
                 self.mode_info.style.colors,
                 self.mode_info.capabilities,
+                tab_index,
             );
 
             is_alternate_tab = !is_alternate_tab;
