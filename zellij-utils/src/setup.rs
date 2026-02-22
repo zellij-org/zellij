8d4d9e818907d380cba40f0e90e2e1a72c11cdb8
divens <divens.dev@gmail.com>
2026-02-22 23:32:36 +0100

    fix: canonicalize expected paths in setup tests for Windows \\?\ prefix

--- zellij-utils/src/setup.rs
+++ zellij-utils/src/setup.rs
@@ -880,7 +880,11 @@ mod setup_test {
         let Some(LayoutInfo::File(layout_path, _)) = layout_info else {
             panic!("layout info has unexpected format");
         };
-        let expected = config_dir.join("layouts").join("upside-down.kdl");
+        let expected = config_dir
+            .join("layouts")
+            .join("upside-down.kdl")
+            .canonicalize()
+            .unwrap();
         assert_eq!(layout_path, expected.display().to_string());
     }
     #[test]
@@ -898,7 +902,11 @@ mod setup_test {
         let Some(LayoutInfo::File(layout_path, _)) = layout_info else {
             panic!("layout info has unexpected format");
         };
-        let expected = config_dir.join("layouts").join("default.kdl");
+        let expected = config_dir
+            .join("layouts")
+            .join("default.kdl")
+            .canonicalize()
+            .unwrap();
         assert_eq!(layout_path, expected.display().to_string());
     }
 
