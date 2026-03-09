900814bdcae94aa07eb7093da8ab841ebc02f787
PilgrimLyieu <PilgrimLyieu@outlook.com>
2026-03-09 14:11:50 +0800

    fix(panes): clear `window_title` cache when switching tabs
    Fix #2614

--- zellij-server/src/panes/tiled_panes/mod.rs
+++ zellij-server/src/panes/tiled_panes/mod.rs
@@ -991,6 +991,7 @@ impl TiledPanes {
             pane.render_full_viewport();
         }
         self.reset_boundaries();
+        self.window_title = None;
     }
     pub fn has_active_panes(&self) -> bool {
         !self.active_panes.is_empty()
