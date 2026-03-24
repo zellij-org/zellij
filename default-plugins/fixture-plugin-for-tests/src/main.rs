3202abcc814ff5b24581f0b3965b2a25178cfe6c
Dawid Pietrykowski <servces+github@dawidpietrykowski.com>
2026-03-24 21:17:17 +0100

    formatting fix

--- default-plugins/fixture-plugin-for-tests/src/main.rs
+++ default-plugins/fixture-plugin-for-tests/src/main.rs
@@ -936,7 +936,10 @@ impl ZellijPlugin for State {
                     self.explicit_string_to_render = Some(format!("Override layout command sent"));
                 },
                 BareKey::Char('l')
-                    if key.has_only_modifiers(&[KeyModifier::Ctrl, KeyModifier::Alt]) => focus_last_pane(),
+                    if key.has_only_modifiers(&[KeyModifier::Ctrl, KeyModifier::Alt]) =>
+                {
+                    focus_last_pane()
+                },
                 _ => {},
             },
             Event::CustomMessage(message, payload) => {
