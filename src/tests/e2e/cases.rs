c2b4c2ac85fa854d103117ea3815b84658199b31
Aram Drevekenin <aram@poor.dev>
2026-03-26 09:01:38 +0100

    fix tests

--- src/tests/e2e/cases.rs
+++ src/tests/e2e/cases.rs
@@ -1149,6 +1149,7 @@ pub fn detach_and_attach_session() {
             instruction: |remote_terminal: RemoteTerminal| -> bool {
                 let mut step_is_complete = false;
                 if remote_terminal.ctrl_plus_appears() && remote_terminal.cursor_position_is(77, 2)
+                    && !remote_terminal.snapshot_contains("Detach")
                 {
                     // we're back inside the session
                     step_is_complete = true;
